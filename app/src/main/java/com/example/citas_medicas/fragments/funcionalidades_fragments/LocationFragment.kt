fe88164eda1722b98e73fb5995ba6cb8b3d6d094
SirLuxord <haendel.tee.2013@gmail.com>
2025-02-07 09:47:10 +0000

    Actualización de como se carga la localización

--- app/src/main/java/com/example/citas_medicas/fragments/funcionalidades_fragments/LocationFragment.kt
+++ app/src/main/java/com/example/citas_medicas/fragments/funcionalidades_fragments/LocationFragment.kt
@@ -9,7 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.citas_medicas.R
 
@@ -17,6 +19,15 @@ class LocationFragment : Fragment() {
 
     private lateinit var locationManager: LocationManager
     private lateinit var textView: TextView
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                getLastKnownLocation()
+            } else {
+                textView.text = "Permiso de ubicación denegado."
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,19 +37,34 @@ class LocationFragment : Fragment() {
         textView = view.findViewById(R.id.text_location)
 
         locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        getLastKnownLocation()
+        checkLocationPermission()
 
         return view
     }
 
+    private fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                getLastKnownLocation()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                textView.text = "Se necesita permiso de ubicación para mostrar la ubicación."
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
     private fun getLastKnownLocation() {
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
             return
         }
         val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
         location?.let {
-            textView.text = "Latitud: ${it.latitude}\nLongitud: ${it.longitude}\nAltitud: ${it.altitude}"
+            textView.text = "Latitud: ${it.latitude}\nLongitud: ${it.longitude}"
+        } ?: run {
+            textView.text = "No se pudo obtener la ubicación."
         }
     }
 }
